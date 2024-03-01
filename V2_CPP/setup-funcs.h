ea5eacf371471f3c89a0d1306a48b354ae7c619e
Agustin Fisher <afisher@itba.edu.ar>
2024-03-01 03:23:33 -0300

    Version nueva/ordenada de TCLib.cpp en lib-setup.cpp

---
+++ V2_CPP/setup-funcs.h
@@ -0,0 +1,16 @@
+#ifndef SETUP_FUNCS_H
+#define SETUP_FUNCS_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+// Function declarations
+void doTheThing();
+void setDarkTheme();
+void setLightTheme();
+void loadCustomComponents();
+
+#endif // SETUP_FUNCS_H
