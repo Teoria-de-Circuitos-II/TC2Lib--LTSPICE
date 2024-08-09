6c1763bf606bcd11da81401ebf9125783b8972f6
gullinoagus <agullino@itba.edu.ar>
2024-08-09 15:27:44 -0300

    Fix for tilde

--- source/setup-funcs.cpp
+++ source/setup-funcs.cpp
@@ -438,7 +438,25 @@ void clear_screen()
     std::system("clear");
 #endif
 }
+#if OS_Windows
+// Convert an ANSI string to a wide Unicode String
+std::wstring ansi2unicode(const std::string &str)
+{
+    int size_needed = MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstrTo(size_needed, 0);
+    MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+    return wstrTo;
+}
 
+// Convert a wide Unicode string to an UTF8 string
+std::string utf8_encode(const std::wstring &wstr)
+{
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+    std::string strTo(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+    return strTo;
+}
+#endif
 bool initLib()
 {
 #if OS_Windows
@@ -484,7 +502,9 @@ bool initLib()
         return true;
     }
 
-    userProfile = std::getenv("USERPROFILE");
+    userProfilewindows = std::getenv("USERPROFILE");
+    std::wstring userProfileunicode = ansi2unicode(userProfilewindows);
+    std::string userProfile = utf8_encode(userProfileunicode);
     userRoaming = userProfile + "\\AppData\\Roaming\\";
     userLTspice = userProfile + "\\AppData\\Local\\LTspice\\";
 
